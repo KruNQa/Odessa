13a58f94d543b90e36092984ccda1fe764483176
KruNQa <playkarusel@gmail.com>
2025-06-07 19:31:28 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const button1 = document.querySelector(".button-1");
-  const button2 = document.querySelector(".button-2");
-
-  button1.addEventListener("click", function () {
-    button2.style.display = "none";
-  });
-});
